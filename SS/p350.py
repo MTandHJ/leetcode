906cb338622b449af4ed479c48f9bc985faf5a9d
LaguaFluc <1754860514@qq.com>
2021-04-22 23:24:50 +0800

    添加自己的代码、官方注解

--- SS/p350.py
+++ SS/p350.py
@@ -32,3 +32,37 @@ class Solution:
             tmp = tmp + [k] * d_s[k]
         
         return tmp
+
+
+# 双指针做法
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # 先排序
+        nums1.sort()
+        nums2.sort()
+
+        # 找出每组的长度
+        length1, length2 = len(nums1), len(nums2)
+        # 用来放共同的都有的数据，也就是交集
+        intersection = list()
+        # 初始化指针（原来就是索引！！！
+        index1 = index2 = 0
+        # 开始遍历循环，直到所有的都遍历完
+        # 总是小的index += 1
+        while index1 < length1 and index2 < length2:
+            if nums1[index1] < nums2[index2]:
+                index1 += 1
+            elif nums1[index1] > nums2[index2]:
+                index2 += 1
+            else:
+                # 他们两个都有，且相等
+                intersection.append(nums1[index1])
+                index1 += 1
+                index2 += 1
+        
+        return intersection
+
+# 作者：demigodliu
+# 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/solution/ha-xi-biao-liang-ge-shu-zu-de-jiao-ji-ii-fkwo/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
